14ed1239b723e4a1ffbd8aa420a08965a1b80db7
eslam-abuelez <105518287+eslam-abuelez@users.noreply.github.com>
2022-10-21 14:55:10 +0200

    Initial commit

---
+++ src/main/java/table/MenuTest.java
@@ -0,0 +1,90 @@
+package table;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MenuTest extends JFrame implements ActionListener {
+    private JMenuBar menuBar;
+    private JMenuItem  fileMenu;
+    private JMenuItem  loadFile;
+    private JMenuItem saveFile;
+    private AbstractButton ta;
+
+
+    public MenuTest(){
+
+        super("Menu Details");
+        menuBar = new JMenuBar();
+        loadFile = new JMenuItem("upload File");
+        saveFile = new JMenuItem("Save File");
+        fileMenu = new JMenu("File");
+
+        loadFile.addActionListener(this);
+        loadFile.setActionCommand("upload File");
+        saveFile.addActionListener(this);
+        saveFile.setActionCommand("save file");
+        fileMenu.add(loadFile);
+
+        fileMenu.add(saveFile);
+        menuBar.add(fileMenu);
+
+
+
+        setJMenuBar(menuBar);
+        setSize(400, 300);
+        setLocation(200,200);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()){
+            case  "upload file" :
+
+                break;
+
+            case "save file":
+
+                break;
+        }
+    }
+
+
+
+    void loadFile(){
+        JFileChooser fc = new JFileChooser();
+       int result=  fc.showOpenDialog(this);
+       if (result== JFileChooser.APPROVE_OPTION) {
+           String path = fc.getSelectedFile().getPath();
+           FileInputStream fis = null;
+           try {
+                fis = new FileInputStream(path);
+               int size = fis.available();
+               byte[] b = new byte[size];
+               fis.read(b);
+               ta.setText(new String(b));
+           } catch (FileNotFoundException e) {
+            e.printStackTrace();
+           }catch (IOException e){
+
+               e.printStackTrace();
+           }finally {
+              try{ fis.close();}catch (IOException e){
+           }
+
+
+       }
+       }
+    //void saveFile(){
+
+    //}
+
+
+    //public static void main(String[] args) {
+        //new MenuTest().setVisible(true);
+   // }
+}}
---
+++ src/main/java/table/TableFrame.java
@@ -0,0 +1,25 @@
+package table;
+
+import javax.swing.*;
+
+public class TableFrame extends JFrame {
+    private JTable InvoiceDetails;
+    private String[]cols= {"No.","Date","Customer","Total"};
+    private  String[][]data={{"0","22-10-22","Islam", "SubZero"},
+            {"0","22-10-22","Islam", "SubZero"}
+            ,{null,null,null, null}
+    };
+
+
+
+    public TableFrame(){
+       // super("Invoice Details");
+
+        super("Invoice Details");
+
+        JTable table = new JTable(data,cols);
+    }
+   // public static void main(String[] args) {
+     //   new TableFrame().setVisible(true);
+   // }
+}
